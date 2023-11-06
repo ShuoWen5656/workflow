1f494da7de7201d642e6db5adf886142c74b6031
ShuoWen5656 <2260755767@qq.com>
2023-11-06 18:06:49 +0800

    [新增代码]1、更新collections工具类

--- workflow-common/src/main/java/top/swzhao/project/workflow/common/utils/CommonUtils.java
+++ workflow-common/src/main/java/top/swzhao/project/workflow/common/utils/CommonUtils.java
@@ -1,9 +1,20 @@
 package top.swzhao.project.workflow.common.utils;
 
+import org.apache.commons.collections.CollectionUtils;
+
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author swzhao
  * @date 2023/10/20 9:30 下午
  * @Discreption <>
  */
 public class CommonUtils {
+
+    public static void main(String[] args) {
+        List<Integer> a = Arrays.asList(1, 1, 1, 1, 1, 1, 2, 3, 4, 5);
+        List<Integer> b = Arrays.asList(1, 1, 1, 1, 2, 3, 8);
+        CollectionUtils.subtract(b, a);
+    }
 }
