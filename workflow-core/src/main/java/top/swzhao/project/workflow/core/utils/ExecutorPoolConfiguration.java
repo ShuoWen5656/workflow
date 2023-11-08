ff18647572cd91d42b6cf2165cc96f784c62a273
ShuoWen5656 <2260755767@qq.com>
2023-11-08 21:59:24 +0800

    [新增代码]1、线程池迁移

---
+++ workflow-core/src/main/java/top/swzhao/project/workflow/core/utils/ExecutorPoolConfiguration.java
@@ -0,0 +1,20 @@
+package top.swzhao.project.workflow.core.utils;
+
+/**
+ * @author swzhao
+ * @data 2023/11/8 21:57
+ * @Discreption <>  线程配置类，建造者
+ */
+public class ExecutorPoolConfiguration {
+
+    /**
+     * 核心线程数 - 10
+     */
+    private int sizeCore = 10;
+
+    private int sizeMax = 20;
+
+
+
+
+}
